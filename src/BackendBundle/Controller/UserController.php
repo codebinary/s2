a45179da9a4f368f75b13cc28588f2359f44d2d2
James Ontiveros <james.ontiveros86@gmail.com>
2017-03-22 19:16:28 -0500

    Creando método para insertar usuario

--- src/BackendBundle/Controller/UserController.php
+++ src/BackendBundle/Controller/UserController.php
@@ -5,6 +5,9 @@ namespace BackendBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use BackendBundle\Entity\User;
+use BackendBundle\Form\UserType;
+
 class UserController extends Controller
 {
     public function loginAction(Request $request){
@@ -33,7 +36,42 @@ class UserController extends Controller
 
     public function createAction(Request $request){
 
-        return $this->render("BackendBundle:User:create.html.twig");
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+
+        
+
+        if($form->isSubmitted() && $form->isValid()){
+            
+            $user = new User();
+            $user->setName($form->get("name")->getData());
+            $user->setSurname($form->get("surname")->getData());
+            $user->setEmail($form->get("email")->getData());
+            $user->setPassword($form->get("password")->getData());
+            $user->setRole('ROLE_USER');
+            $user->setImage(null);
+            $user->setCreatedAt(new \DateTime('now'));
+
+            $em = $this->getDoctrine()->getEntityManager();
+            $em->persist($user);
+            $flush = $em->flush();
+
+            if($flush){
+                $status  = "El usuario se ha creado correctamente";
+            }else{
+                $status = "No te has registrado correctamente";
+            }
+            
+        }else{
+            $status = "No te has registrado correctamente";
+        }
+
+        return $this->render("BackendBundle:User:create.html.twig", array(
+            "form" => $form->createView(),
+            "status" => $status
+        ));
 
     }
 }
--- src/BackendBundle/Form/UserType.php
+++ src/BackendBundle/Form/UserType.php
@@ -6,6 +6,11 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
 class UserType extends AbstractType
 {
     /**
@@ -15,13 +20,21 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('role')
-            ->add('name')
-            ->add('surname')
-            ->add('email')
-            ->add('password')
-            ->add('image')
-            ->add('createdAt', 'datetime')
+            ->add('name', TextType::class, array("label"=>"Nombre", "required" => "required", "attr" => array(
+                "class"=>"form-control col-md-7 col-xs-12"
+            )))
+            ->add('surname', TextType::class, array("label"=>"Apellido", "required" => "required", "attr" => array(
+                "class"=>"form-control col-md-7 col-xs-12"
+            )))
+            ->add('email', EmailType::class, array("label"=>"Email", "required" => "required", "attr" => array(
+                "class"=>"form-control col-md-7 col-xs-12"
+            )))
+            ->add('password', PasswordType::class, array("label"=>"Contraseña", "required" => "required", "attr" => array(
+                "class"=>"form-control col-md-7 col-xs-12"
+            )))
+            ->add('Guardar', SubmitType::class, array("attr" => array(
+                "class"=>"form-control col-md-7 col-xs-12 btn btn-success"
+            )))
         ;
     }
     
